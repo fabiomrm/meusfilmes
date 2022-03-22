14146b68d7c3c3b5093afdb9b1f4be5750f1fe7a
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-03-22 11:20:32 -0300

    add frontend folder and use herokus backend

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 import { getAuthData } from './storage';
 
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'https://movieflix-devsuperior.herokuapp.com';
 export const CLIENT_ID = 'myclientid';
 export const CLIENT_SECRET = 'myclientsecret';
 
